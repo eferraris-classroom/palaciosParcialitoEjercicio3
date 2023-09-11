515e1ee6b495980265fc628b216f05cfa59aef39
Agustin <agustin@ini.live>
2023-09-11 09:18:06 -0300

    ADD comments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Ejercicio 3
 ## Enunciado
 
-3. Hacer un programa que inicialice dos listas y muestre por pantalla la intersección entre las mismas.# palaciosParcialitoEjercicio3
+3. Hacer un programa que inicialice dos listas y muestre por pantalla la intersección entre las mismas.
--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,18 +1,18 @@
 package ar.edu.undef.fie;
 
-// Hacer un programa que inicialice dos listas y muestre por pantalla la intersección entre las mismas.
 public class Main {
     public static void main(String[] args) {
-        int[] lista1 = {1, 2, -4, 8, 5, 6, 99};
-        int[] lista2 = {1, 3, 5, 8, 99, 9, -4};
+        int[] lista1 = {1, 2, -4, 8, 5, 6, 99};     // Inicializo lista 1
+        int[] lista2 = {1, 5, 99, 9, -4, 2, 0};     // Inicializo lista 2
 
         interseccion(lista1, lista2);
     }
 
+    // Funcion que imprime la interseccion de dos listas
     private static void interseccion(int[] lista1, int[] lista2) {
         for (var numero1 : lista1) {
             for (var numero2 : lista2) {
-                if (numero1 == numero2) System.out.println(numero1);
+                if (numero1 == numero2) System.out.println(numero1); //En este caso solo muestro, como pide el enunciado
             }
         }
     }
